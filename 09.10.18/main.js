974365bf40c8e961edc7c51d75ecf56da4af64eb
VolodymyrBohuslavskiy <936187435BV@gmail.com>
2018-10-09 15:41:44 +0300

    Mas.+reverse HW

---
+++ 09.10.18/main.js
@@ -0,0 +1,109 @@
+let num_1 = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// 1
+/*let i_1=0;
+ while (i_1<num_1.length){
+ console.log(num_1[i_1]);
+ i_1++}*/
+
+// 2
+/*
+ for (let i = 0; i < num_1.length; i++) {
+ let obj_2 = num_1[i];
+ console.log(obj_2);
+ }*/
+
+// 3
+/*let i_1=0;
+ while (i_1<num_1.length){
+ if (i_1%2==!0)
+ console.log(num_1[i_1]);
+ i_1++}*/
+
+// 4
+/*
+ for (let i = 0; i < num_1.length; i++) {
+ let obj = num_1[i];
+ if (i%2==0)console.log(obj)
+ */
+
+// 5
+/*
+ let i_1=0;
+ while (i_1<num_1.length){
+ if (num_1[i_1]%2==0)
+ console.log(num_1[i_1]);
+ i_1++}
+ */
+
+
+//6
+/*
+ for (let i = 0; i < num_1.length; i++) {
+ let obj = num_1[i];
+ if (num_1[i]%2==0)console.log(obj)
+ }
+ */
+
+
+//7
+/*for (let i = 0; i < num_1.length; i++) {
+ let obj = num_1[i];
+ if (obj % 3 == 0) num_1[i] = 'okten';
+ console.log(num_1[i])}*/
+
+
+//8
+/*
+ let re_num_1=num_1.reverse();
+ console.log(re_num_1);
+ */
+
+
+//9
+//7-R
+/*for (let i = num_1.length - 1; i >= 0; i--) {
+ if (num_1[i] % 3 == 0) num_1[i] = 'okten';
+ console.log(num_1[i])
+ }*/
+
+
+// 6r
+/*for (let i = num_1.length - 1; i >= 0; i--) {
+ let obj = num_1[i];
+ if (num_1[i]%2==0)console.log(obj)
+ }*/
+
+
+// 5r
+/*
+ let i=num_1.length;
+ while (i>=0){
+ if (num_1[i]%2==0)
+ console.log(num_1[i]);
+ i--}
+ */
+
+// 4r
+/*for (let i = num_1.length - 1; i >= 0; i--) {
+ let obj = num_1[i];
+ if (i%2==0)console.log(obj)}
+ */
+
+// 3r
+/*let i = num_1.length - 1;
+while (i >= 0) {
+    if (i % 2 == 0)
+        console.log(num_1[i]);
+    i--}*/
+
+//2r
+/*for (let i = num_1.length - 1; i >= 0; i--) {
+    let obj_2 = num_1[i];
+    console.log(obj_2);}
+    */
+
+// 1r
+/*let i = num_1.length - 1;
+while (i >= 0) {
+    console.log(num_1[i]);
+    i--}*/
